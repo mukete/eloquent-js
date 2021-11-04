918f085b39039189b417d1ce5d1258be2f60d7b5
mukete <mukete@yandex.com>
2021-11-04 02:36:52 +0100

    fizzbuzz part 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,11 +5,13 @@ for(let i = 1; i <= 100; i++) {
 		console.log('Fizz');
 	}
 
-	else if(i%5 == 0) {
+	if(i%5 == 0) {
 		console.log('Buzz');
 	}
 
-	
+	if(i%5 == 0 && i%3 == 0) {
+		console.log('FizzBuzz');
+	}
 
 	else {
 		console.log(i);
